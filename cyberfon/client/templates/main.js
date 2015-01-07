243dcb6c8fce78ade9bb3ebb18ba823a88848bcd
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-01-07 17:40:03 +0300

    status editing

--- cyberfon/client/templates/main.js
+++ cyberfon/client/templates/main.js
@@ -1,16 +1,28 @@
+var STATUS_EDITIING = 'Template.main.status_editing';
+Session.setDefault(STATUS_EDITIING, false);
+
 Template.main.helpers({
     'status': function() {
         var user = Meteor.user();
-        if (user === undefined) {
-            return undefined;
-        }
-        if (user.profile === undefined) {
-            user.profile = {'status': '??'};
-            Meteor.users.update({'_id': user._id}, {$set: {'profile': user.profile}})
-        }
-        return user.profile.status;
+        return (user === undefined) ? undefined : user.profile.status;
     },
     'users': function() {
         return Meteor.users.find({});
+    },
+    'status_editing': function() {
+        return Session.get(STATUS_EDITIING);
+    }
+});
+
+Template.main.events({
+    'click .js-edit-status': function(event) {
+        Session.set(STATUS_EDITIING, true);
+    },
+    'keyup input[type=text]': _.throttle(function(event) {
+        var user = Meteor.user();
+        Meteor.users.update({'_id': user._id}, {$set: {'profile.status': event.target.value}})
+    }, 300),
+    'blur input[type=text]': function(event) {
+        Session.set(STATUS_EDITIING, false);
     }
 });

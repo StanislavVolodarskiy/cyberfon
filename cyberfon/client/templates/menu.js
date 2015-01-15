8da755f98ff0863e5a9d52c2d395c0ce91c72b44
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-01-15 21:38:48 +0300

    some styles

--- cyberfon/client/templates/menu.js
+++ cyberfon/client/templates/menu.js
@@ -1,5 +1,26 @@
 Template.menu.helpers({
     'username': function() {
         return Meteor.user().username;
+    },
+    'lat': function() {
+        
+        var each = function(obj, f) {
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    f(key, obj[key]);
+                }
+            }
+        };
+        var dump = function(title, obj) {
+            console.log(title);
+            each(obj, function(key, value) {
+                console.log('   ' + key + ': ' + value);
+            });
+        };
+        dump('error', Geolocation.error());
+        dump('location', Geolocation.currentLocation());
+        dump('coords', Geolocation.currentLocation().coords);
+        dump('latLng', Geolocation.latLng());
+        return 'UGU';
     }
 });

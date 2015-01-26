dded19e9d994322c9af9032a9b775811f55b5767
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-01-26 22:56:59 +0300

    sign_in

--- cyberfon/client/templates/sign_in.js
+++ cyberfon/client/templates/sign_in.js
@@ -1,15 +1,34 @@
+var SIGN_IN_ERRORS = 'Template.sign_in.errors';
+
+Template.sign_in.created = function() {
+    Session.set(SIGN_IN_ERRORS, []);
+};
+
+Template.sign_in.helpers({
+    errors: function() {
+        return Session.get(SIGN_IN_ERRORS);
+    }
+});
+
 Template.sign_in.events({
     'submit': function(event, template) {
         event.preventDefault();
 
-        var username = template.$('[name=username]').val();
-        var password = template.$('[name=password]').val();
+        var phone_num = template.$('[name=phone_num]').val();
+        var password  = template.$('[name=password]').val();
 
-        Meteor.loginWithPassword({'username': username}, password, function(error) {
+        Meteor.loginWithPassword({'username': phone_num}, password, function(error) {
             if (error === undefined) {
                 Router.go('main');
             } else {
                 console.log(error);
+
+                var errors = [];
+
+                if (error.reason == 'Match failed') {
+                    errors.push('Нет такой пары телефон/пароль.');
+                }
+                Session.set(SIGN_IN_ERRORS, errors);
             }
         });
     }

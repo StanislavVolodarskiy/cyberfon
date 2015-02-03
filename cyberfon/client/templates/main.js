97e77b9e333273c819a938807e851ac55d375712
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-02-04 01:09:28 +0300

    user distance and favorite

--- cyberfon/client/templates/main.js
+++ cyberfon/client/templates/main.js
@@ -13,8 +13,6 @@ Template.main.helpers({
             'date'     : new Date(),
             'nComments': 3
         };
-        var user = Meteor.user();
-        return (user === undefined || user === null) ? undefined : user.profile.status;
     },
 
     'users': function() {
@@ -37,14 +35,34 @@ Template.main.helpers({
             }).map(function(doc) { return doc.user; });
         };
 
+        var make_set = function(array) {
+            var dict = {};
+            _.each(array, function(v) {
+                dict[v] = undefined;
+            });
+            return function(v) {return dict.hasOwnProperty(v);};
+        };
+
         var user = Meteor.user();
         if (user === undefined || user === null) {
             return [];
         }
 
-        var user_ids = user.profile.favorites.concat(neighbours(user._id, 1000));
+        var neighbour_ids = neighbours(user._id, 1000);
+        var user_ids = user.profile.favorites.concat(neighbour_ids);
 
-        return Meteor.users.find({'_id': {'$in': user_ids}});
+        var neighbour_set = make_set(neighbour_ids         );
+        var favorite_set  = make_set(user.profile.favorites);
+
+        return Meteor.users.find({
+            '_id': {'$in': user_ids}
+        }).map(function(doc) {
+            var user = doc.profile;
+            user.fake_avatar = 'avatars/av1.jpg';
+            user.distance = neighbour_set(doc._id) ? 'near' : 'far';
+            user.favorite = favorite_set (doc._id) ? 'yes'  : 'no' ;
+            return user;
+        });
     }
 });
 

73392b4b51d2e8d066a084e9bd3582e35e45d1b6
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-02-19 14:54:34 +0300

    layout_main

--- cyberfon/client/templates/layout_main.js
+++ cyberfon/client/templates/layout_main.js
@@ -4,7 +4,13 @@ Template.layout_main.helpers({
     }
 });
 Template.layout_main.events({
-    'click .js-back': function(event) {
+    'click .js-back': function() {
         window.history.back();
+    },
+    'click .js-open-favorites': function() {
+        Router.go('favorites');
+    },
+    'click .js-open-dialogs': function() {
+        Router.go('dialogs');
     }
 });

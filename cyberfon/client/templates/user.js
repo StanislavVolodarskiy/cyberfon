530cfbb01dce4c8cbe5e45fd3137cfa727b00714
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-02-19 14:25:32 +0300

    user

--- cyberfon/client/templates/user.js
+++ cyberfon/client/templates/user.js
@@ -2,6 +2,16 @@ Template.user.helpers({
     'profile': function() {
         var user = Meteor.users.findOne({'_id': this.user_id}, {'profile': 1});
         return (user === undefined) ? undefined : user.profile;
+    },
+    'statuses': function() {
+        var user = Meteor.users.findOne({'_id': this.user_id}, {'profile': 1});
+        var status = (user === undefined) ? undefined : user.profile.status;
+
+        return Statuses.find({'user': this.user_id}, {'user_id': 0}).map(function(doc) {
+            doc['active_class'] = (doc._id === status) ? 'twit_active' : 'twit_passive';
+            doc['n_comments'] = Comments.find({'status': doc._id}).count();
+            return doc;
+        });
     }
 });
 

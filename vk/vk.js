d05ef9a6f74b5550b386e3fe62b8d3ae23acd148
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-05-23 18:39:09 +0300

    vk

--- vk/vk.js
+++ vk/vk.js
@@ -1,5 +1,10 @@
 if (Meteor.isClient) {
     (function() {
+        var auth_cb_url = function() {
+            // return 'https://oauth.vk.com/blank.html';
+            return Meteor.settings.public.scheme + '://' + Meteor.settings.public.host + '/auth_cb';
+        };
+
         var url = function(path, params) {
             if (params === undefined) {
                 params = [];
@@ -13,19 +18,24 @@ if (Meteor.isClient) {
         Template.main.helpers({
             'auth_url': function() {
                 return url('https://oauth.vk.com/authorize', [
-                    ['client_id', '4910052'],
-                    ['scope', 'friends'],
-                    ['redirect_uri', 'https://oauth.vk.com/blank.html'],
-                    ['display', 'page'],
-                    ['v', '5.32'],
-                    ['response_type', 'token']
+                    ['client_id'    , '4910052'    ],
+                    ['scope'        , 'friends'    ],
+                    ['redirect_uri' , auth_cb_url()],
+                    ['display'      , 'page'       ],
+                    ['v'            , '5.32'       ],
+                    ['response_type', 'token'      ]
                 ]);
+            },
+            'settings': function() {
+                return JSON.stringify(Meteor.settings.public);
             }
         });
     })();
 }
 
 if (Meteor.isServer) {
+    Meteor.methods();
+
     Meteor.startup(function() {
         // code to run on server at startup
     });

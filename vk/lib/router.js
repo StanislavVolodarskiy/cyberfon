ad5c566946e97e1d3f147999f168c41c7e16adef
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-05-29 21:25:17 +0300

    auth again

--- vk/lib/router.js
+++ vk/lib/router.js
@@ -1,12 +1,50 @@
 var vk_access = (function() {
     var user;
     var token;
+
+    var auth_cb_route = '/auth_cb';
+
+    Router.route(auth_cb_route, function() {
+        var map = {};
+        _.each(this.params.hash.split('&'), function(v) {
+            var items = v.split('=');
+            map[items[0]] = items[1];
+        });
+        console.log(map);
+        user = map.user_id;
+        token = map.access_token;
+        Router.go(decodeURIComponent(map.state));
+    });
+
+    var authorize = function(go_to) {
+        var auth_cb_url = function() {
+            return Meteor.settings.public.scheme + '://' + Meteor.settings.public.host + '/' + auth_cb_route;
+        };
+
+        var url = function(path, params) {
+            if (params === undefined) {
+                params = [];
+            }
+            var param_str = _.map(params, function(v) {
+                return v[0] + '=' + encodeURIComponent(v[1]);
+            }).join('&');
+            return (param_str === '') ? path : path + '?' + param_str;
+        };
+
+        window.open(url('https://oauth.vk.com/authorize', [
+            ['client_id'    , '4910052'    ],
+            ['scope'        , 'friends'    ],
+            ['redirect_uri' , auth_cb_url()],
+            ['display'      , 'page'       ],
+            ['v'            , '5.32'       ],
+            ['response_type', 'token'      ],
+            ['state'        , go_to        ]
+        ]), '_self');
+    };
+
     return {
-        'open': function(u, t) {
-            user = u;
-            token = t;
-        },
-        'opened': function() {
+        'authorize': authorize,
+        'authorized': function() {
             return token !== undefined;
         }
     };
@@ -21,53 +59,16 @@ Router.configure({
 Router.route('root', function() {}, {
     'path': '/',
     'onBeforeAction': function() {
-        Router.go('auth');
+        Router.go('friends');
         this.next();
     }
 });
 
 Router.route('friends', {
     'onBeforeAction': function() {
-        if (!vk_access.opened()) {
-            Router.go('auth');
+        if (!vk_access.authorized()) {
+            vk_access.authorize('friends');
         }
         this.next();
     }
-});
-
-Router.route('auth', function() {
-    var auth_cb_url = function() {
-        return Meteor.settings.public.scheme + '://' + Meteor.settings.public.host + '/auth_cb';
-    };
-
-    var url = function(path, params) {
-        if (params === undefined) {
-            params = [];
-        }
-        var param_str = _.map(params, function(v) {
-            return v[0] + '=' + encodeURIComponent(v[1]);
-        }).join('&');
-        return (param_str === '') ? path : path + '?' + param_str;
-    };
-
-    window.open(url('https://oauth.vk.com/authorize', [
-        ['client_id'    , '4910052'    ],
-        ['scope'        , 'friends'    ],
-        ['redirect_uri' , auth_cb_url()],
-        ['display'      , 'page'       ],
-        ['v'            , '5.32'       ],
-        ['response_type', 'token'      ],
-        ['state'        , 'UGU'        ]
-    ]), '_self');
-});
-
-Router.route('auth_cb', function() {
-    var map = {};
-    _.each(this.params.hash.split('&'), function(v) {
-        var items = v.split('=');
-        map[items[0]] = items[1];
-    });
-    console.log(map);
-    vk_access.open(map.user_id, map.access_token);
-    Router.go('friends');
 });

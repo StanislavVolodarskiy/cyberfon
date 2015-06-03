acd09b6113e2f9cbbef118767381a3de7bac4ae5
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-06-03 11:14:43 +0300

    vk login

--- vk/lib/router.js
+++ vk/lib/router.js
@@ -7,46 +7,6 @@ Router.configure({
 var vk = (function(auth_cb_route) {
     var version = '5.33';
 
-    var user;
-    var token;
-
-    Router.route(auth_cb_route, function() {
-        var map = {};
-        _.each(this.params.hash.split('&'), function(v) {
-            var items = v.split('=');
-            map[items[0]] = items[1];
-        });
-        user = map.user_id;
-        token = map.access_token;
-        Router.go(map.state);
-    });
-
-    var url = function(path, params) {
-        if (params === undefined) {
-            params = [];
-        }
-        var param_str = _.map(params, function(v) {
-            return v[0] + '=' + encodeURIComponent(v[1]);
-        }).join('&');
-        return (param_str === '') ? path : path + '?' + param_str;
-    };
-
-    var login = function(go_to) {
-        var auth_cb_url = function() {
-            return Meteor.settings.public.scheme + '://' + Meteor.settings.public.host + '/' + auth_cb_route;
-        };
-
-        window.open(url('https://oauth.vk.com/authorize', [
-            ['client_id'    , '4910052'    ],
-            ['scope'        , 'friends'    ],
-            ['redirect_uri' , auth_cb_url()],
-            ['display'      , 'page'       ],
-            ['v'            , version      ],
-            ['response_type', 'token'      ],
-            ['state'        , go_to        ]
-        ]), '_self');
-    };
-
     var users = function(cb) {
         HTTP.get(url('https://api.vk.com/method/users.get', [
             ['v'           , version    ],
@@ -56,10 +16,6 @@ var vk = (function(auth_cb_route) {
     };
 
     return {
-        'login': login,
-        'loggedIn': function() {
-            return token !== undefined;
-        },
         'users': users
     };
 })('vk_auth_cb');
@@ -74,12 +30,13 @@ Router.route('root', function() {}, {
 
 Router.route('friends', {
     'onBeforeAction': function() {
-        if (!vk.loggedIn()) {
-            vk.login('friends');
-        } else {
-            vk.users(function(response) {
-                console.log(response);
+        if (Meteor.user() === null) {
+            console.log('THERE');
+            Meteor.loginWithVk(function() {
+                console.log(arguments);
             });
+        } else {
+            console.log('HERE');
         }
         this.next();
     }
---
+++ vk/server/server.js
@@ -0,0 +1,11 @@
+
+ServiceConfiguration.configurations.remove({
+    'service': 'vk'
+});
+
+ServiceConfiguration.configurations.insert({
+    'service': 'vk',
+    'appId'  : '4910052',
+    'secret' : '3GT1pXVf5eXkArMoXalz'
+});
+
